2c0fb4cc02a2a53deaffe9e9ef2dab6ad8ee8789
James Allen <jacques2186@yahoo.com>
2019-10-24 20:19:17 -0400

    adjusted config test file

--- app/Schedule/config_test.php
+++ app/Schedule/config_test.php
@@ -1,8 +1,42 @@
 <?php
 
 $config = [
-		"monday" => [
-				"id"=>1,
-				"shift"=>"0700-1530"
-		]
+		"0700-1500" => [
+				"Monday" => [
+						"id" => 1,
+						"tech"=>true,
+						"shift"=>"first"
+				],
+				"Tuesday" => [
+						"id" =>2, 
+						"tech"=>true,
+						"shift"=>"first"
+				],
+				"Wednesday" => [
+						"id" =>3, 
+						"tech"=>true,
+						"shift"=>"first"
+				],
+				"Thursday" => [
+						"id" =>4,
+						"tech"=>true,
+						"shift"=>"first"
+				],
+				"Friday" => [
+						"id" =>5,
+						"tech"=>true,
+						"shift"=>"first"
+				],
+				"Saturday" => [
+						"id" =>6,
+						"tech"=>true,
+						"shift"=>"first"
+				],
+				"Sunday" => [
+						"id" =>7,
+						"tech"=>true,
+						"shift"=>"first"
+				],
+				
+		],
 ];
--- tests/Unit/ScheduleConfigTest.php
+++ tests/Unit/ScheduleConfigTest.php
@@ -13,14 +13,48 @@ class ScheduleConfigTest extends TestCase
 	{
 	   	include app_path() . '/Schedule/config_test.php'; 
 		$configtest = [
-				"monday" => [
-						"id"=>1,
-						"shift"=>"0700-1530"
-				]
+				"0700-1500" => [
+						"Monday" => [
+								"id" => 1,
+								"tech"=>true,
+								"shift"=>"first"
+						],
+						"Tuesday" => [
+								"id" =>2, 
+								"tech"=>true,
+								"shift"=>"first"
+						],
+						"Wednesday" => [
+								"id" =>3, 
+								"tech"=>true,
+								"shift"=>"first"
+						],
+						"Thursday" => [
+								"id" =>4,
+								"tech"=>true,
+								"shift"=>"first"
+						],
+						"Friday" => [
+								"id" =>5,
+								"tech"=>true,
+								"shift"=>"first"
+						],
+						"Saturday" => [
+								"id" =>6,
+								"tech"=>true,
+								"shift"=>"first"
+						],
+						"Sunday" => [
+								"id" =>7,
+								"tech"=>true,
+								"shift"=>"first"
+						],
+						
+				],
 		];
 
 		$this->assertEquals($config, $configtest);
-		$this->assertEquals($config['monday']['id'], 1);
+		$this->assertEquals($config['0700-1500']['Monday']['id'], 1);
 		
 	}
 }
